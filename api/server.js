2c091789061f4d985a19fd8e1eadc9e9eab7864e
sooof <sooof@me.com>
2022-02-06 10:19:25 -0800

    get hello test

--- api/server.js
+++ api/server.js
@@ -3,4 +3,10 @@ const express = require('express');
 
 const server = express()
 server.use(express.json())
+
+// TEST ok: http get :9000/hello
+server.get('/hello', (req, res)=>{
+    res.json("hello world")
+})
+
 module.exports = server; // EXPORT YOUR SERVER instead of {}
